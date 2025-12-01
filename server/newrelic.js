d4d91a65d2eaf7465f38e1ddd77b4fdea5739e24
zameer fouzan <zfouzan@newrelic.com>
2025-12-01 22:52:09 +0530

    Redact license keys and prepare for migration

--- server/newrelic.js
+++ server/newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
     /**
      * Your New Relic license key.
      */
-    license_key: '***REMOVED***',
+    license_key: '<INSERT YOUR KEY>',
     logging: {
         /**
          * Level at which to log. 'trace' is most useful to New Relic when diagnosing
